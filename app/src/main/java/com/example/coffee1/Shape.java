a9acc8c4f2448968f82a5e4aea3aa97c07f846aa
ravyn <16183076+ravynhardcastle@users.noreply.github.com>
2024-09-26 19:19:10 -0600

    Adds blue colour.

--- app/src/main/java/com/example/coffee1/Shape.java
+++ app/src/main/java/com/example/coffee1/Shape.java
@@ -3,6 +3,7 @@ package com.example.coffee1;
 public abstract class Shape {
     private int x;
     private int y;
+    String color = "blue";
 
     public Shape(int y, int x) {
         this.y = y;
